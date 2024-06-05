ac55f82112bc4eefcc77e3e772ca4127612c1cb1
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2024-06-06 00:16:08 +0700

    Successfully read data from MongoDB

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDb = async function () {
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGO_DB_URL);
+    console.log(`MongoDB Connected ${connection.host}`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = connectDb;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+interface ItemsTypes {
+  _id: string;
+  title: string;
+  description: string;
+  created_at: Date;
+}
 
 const App: React.FC = () => {
-  return <div>Todo List</div>;
+  const [items, setItems] = useState<ItemsTypes[]>();
+  useEffect(() => {
+    async function getItems() {
+      const { data } = await axios.get("http://localhost:3000/");
+      console.log(data);
+      setItems(data);
+      return data;
+    }
+    getItems();
+  }, []);
+  return (
+    <ul>
+      {items?.map((item) => {
+        return (
+          <li>
+            {item.title}
+            <br></br>
+            {item.description}
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default App;
